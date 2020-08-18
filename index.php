559877bfc7604d2976dd697713d3b8944e64f6c0
asc3rr <borysgnacinski@gmail.com>
2020-08-18 23:37:28 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+
+    <title>sendbyte.pl - Newsy ze świata IT, właśnie dla Ciebie!</title>
+
+    <link rel="stylesheet" href="css/all.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    <div id="logo">
+        <a id="login-button" href=".">sendbyte.pl - Newsy ze świata IT</a>
+        <div id="buttons">
+            <a class="button" href="/">Najnowsze wpisy</a>
+            <a class="button" href="szukajka/">Szukajka</a>
+            <a class="button" href="kontakt/">Kontakt</a>
+        </div>
+    </div>
+</body>
+</html>
